1e05f92a0d4fb1334c3876ae75953e3ef19a9562
Kimikhwan <moonfgg@naver.com>
2026-05-18 11:12:15 +0900

    fix: 몬스터 스폰, 플레이어 참조, 초기화 플로우

--- Assets/02_Scripts/Manager/InGameCore.cs
+++ Assets/02_Scripts/Manager/InGameCore.cs
@@ -1,11 +1,41 @@
-
 using UnityEngine;
 
 public class InGameCore : MonoBehaviour
 {
+    GameObject player;
+    EnemySpawnManager enemySpawnManager;
+
+    void Awake()
+    {
+        // TODO: 플레이어 소환 및 캐싱
+        // TODO: 몬스터 스포너 동작?
+        Init();
+    }
 
     void Update()
     {
         GameManager.UI.GetPresenter<GameHUDPresenter, GameHUDView>().AddTime(Time.deltaTime);
+    }
+
+    void Init()
+    {
+        PlayerSpawn();
+        EnemySpawn();
+    }
+
+    void PlayerSpawn()
+    {
+        player = Instantiate(Resources.Load<GameObject>("Player"));
+    }
+
+    void EnemySpawn()
+    {
+        if(null == player)
+        {
+            Debug.LogError("Player is not spawned. Enemy spawn failed.");
+            return;
+        }
+
+        enemySpawnManager = new(player.transform);
     }
 }
